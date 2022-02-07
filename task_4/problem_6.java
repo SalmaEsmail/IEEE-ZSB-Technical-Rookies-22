e6350962437e712c27c7c5c18e1b0942f3c16a6d
Salma Esmail <90252790+SalmaEsmail@users.noreply.github.com>
2022-02-07 22:53:00 +0200

    Problem_

--- task_4/problem_6.java
+++ task_4/problem_6.java
@@ -1,1 +1,40 @@
 
+import java.util.Scanner;
+
+public class JavaApplication20 {
+
+   
+    public static void main(String[] args) {
+         Scanner  input =new Scanner(System.in);
+         int j=0;
+        int t =input.nextInt();
+        while(j<t)
+        {
+            int n=input.nextInt();
+            int c=input.nextInt();
+            int m=input.nextInt();
+            int s=0;
+            int z1=n/c;
+            int z2;
+            s=s+z1;
+            if(z1>=m )
+            {
+                while(z1!=1 || z1!=0)
+                {
+                    z2=z1%m;
+                    z1=z1/m;
+                    s=s+z1;
+                    z1=z1+z2;
+                    if(z1<m)
+                    {
+                        break;
+                    }
+                }
+            }
+            System.out.println(s);
+            j++;
+        }
+    }
+    
+}
+
