8b14a2ee90b2d4dd726a80f81713a2b46bcd73f4
Salma Esmail <90252790+SalmaEsmail@users.noreply.github.com>
2022-02-08 16:42:11 +0200

    Update problem_1.java

--- task_4/problem_1.java
+++ task_4/problem_1.java
@@ -1,1 +1,31 @@
+import java.util.Scanner;
 
+
+public class Task_41 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+             int size = input.nextInt();
+            int num=0;
+            int i ,j,d =0;
+	    int arr [] = new int [size]; 
+	    for ( i = 0 ;i < size ; i++ ) {
+	        arr[i] = input.nextInt(); 
+
+	    }
+           for (i = 0; i < size; i++) {
+        for (j = i + 1; j < size; j++) {
+            if(arr[i]==arr[j])
+               {
+                 d= (j-i);
+                 if(d <= size) {
+                     size = d;
+               }
+        
+    }
+    }
+    
+}
+           System.out.println(d);
+    }
+}
