65f8d535072620d9903214827abdaffbcba393fc
Salma Esmail <90252790+SalmaEsmail@users.noreply.github.com>
2022-02-11 12:55:20 +0200

    Update porblem_2.java

--- task_4/porblem_2.java
+++ task_4/porblem_2.java
@@ -1,1 +1,33 @@
+import java.util.*;
 
+public class Task4_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter you letters : ");
+        String s = input.next();
+        int repeat = input.nextInt();
+        String m ;
+      int numr = 0;
+        int numll = 0;
+        
+        for(int i = 0;i<repeat;i++)
+        {
+            m=m+ s;
+            }
+        for(int i = 0;i<repeat;i++)
+        {
+         
+            if(sum.charAt(i)=='r')
+           
+            numr ++;
+            
+        }
+        System.out.println(numr);
+
+    }
+
+}
