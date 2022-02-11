9a9060ba0563324a6ac02da46009da10cb2860ff
Salma Esmail <90252790+SalmaEsmail@users.noreply.github.com>
2022-02-11 22:08:14 +0200

    Update problem_3.cpp

--- task_5/problem_3.cpp
+++ task_5/problem_3.cpp
@@ -1,1 +1,20 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,k,q;
+	cin>>n>>k>>q;
+	int arr[n];
+	int que[q];
+	for(int i=0;i<n;i++)
+	{
+	  cin>>arr[(k+i)%n];
+    }
+    for(int i=0;i<q;i++)
+    cin>>que[i];
+    for(int i=0;i<q;i++)
+    {
+      cout<<arr[que[i]]<<endl;
+    }
+    return 0;
+}
