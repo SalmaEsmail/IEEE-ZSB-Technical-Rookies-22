28ca21b8d49a851c61fdb10271e9893077a0dd9b
Salma Esmail <90252790+SalmaEsmail@users.noreply.github.com>
2022-02-14 19:12:42 +0200

    Update problem_2.java

--- task_5/problem_2.java
+++ task_5/problem_2.java
@@ -4,14 +4,13 @@ public class Task_4_2 {
 
    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int v;
         int n =  input.nextInt();
         int x =  input.nextInt();
         int sum = 0;
         int k = 0;
         while (true) {
            x--;
-            v =  input.nextInt();
+          int  v =  input.nextInt();
             sum = (86400 - v) + sum;
             k++;
             if (n <= sum) {
