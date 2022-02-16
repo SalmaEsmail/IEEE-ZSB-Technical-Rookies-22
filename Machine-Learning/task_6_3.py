08a59675d0c206b60ee3c49271465f433979c285
Salma Esmail <90252790+SalmaEsmail@users.noreply.github.com>
2022-02-16 23:26:10 +0200

    Update task_6_3.py

--- Machine-Learning/task_6_3.py
+++ Machine-Learning/task_6_3.py
@@ -6,7 +6,6 @@ from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 
 import pickle
-
 pickle_in = open("X.pickle","rb")
 X = pickle.load(pickle_in)
 
@@ -14,9 +13,7 @@ pickle_in = open("y.pickle","rb")
 y = pickle.load(pickle_in)
 
 X = X/255.0
-
 model = Sequential()
-
 model.add(Conv2D(256, (3, 3), input_shape=X.shape[1:]))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
